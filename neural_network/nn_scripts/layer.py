99cbde263cfd4074f9b5da46b3c1a8f074506a63
jambory <cobyw16@gmail.com>
2024-05-09 10:39:21 -0700

    neural_network

---
+++ neural_network/nn_scripts/layer.py
@@ -0,0 +1,10 @@
+class Layer:
+    def __init__(self):
+        self.input=None
+        self.output=None
+    def forward(self, input):
+        # To do: forward propagate with given input
+        pass
+    def backward(self, output_gradient, learning_rate):
+        # TODO : propagate backwards
+        pass
