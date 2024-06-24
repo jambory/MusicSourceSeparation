19725ec8cdb5fc38d1e23dd99ad3f8528fc3bc83
jambory <cobyw16@gmail.com>
2024-06-24 14:32:26 -0700

    updated file names and made reports a bit better

---
+++ mss_nn_implementation/convtas.py
@@ -0,0 +1,36 @@
+from layer import Encoder, Separator, Decoder
+from tensorflow.keras import models
+from parameters import Parameters
+
+# I heavily relied upon, upon Jane Wu's TensorFlow implementation (https://github.com/JaneWuNEU/Conv-TasNet-2) to get this to work.
+# nplab's and kaituoxu's PyTorch implementations of the model were also instrumental for my understanding of the model.
+    
+class ConvTasNet(models.Model):
+    """
+    Full Conv-TasNet model, coded in TensorFlow. I heavily relied upon, upon Jane Wu's TensorFlow implementation (https://github.com/JaneWuNEU/Conv-TasNet-2)
+    to get this to work. nplab's and kaituoxu's PyTorch implementations of the model were also instrumental for my understanding of the model.
+    
+    To explain what the model is doing in the most basic way, it takes a dataset of overlapping chunks of a audio in a waveform format then 
+    encodes them using a simple 1D convolution. With this input data, the model is then put through several iterations of downsampling to get 
+    features from the data. These features are then turned into separate masks for each target source in the audio data. Finally the masks are 
+    applied to the orginal encoded data, which is finally put back into its orginal waveform data format with a Transpose 1D convolution.
+
+    To understand the shape of the data throughout the model, I highly recommend looking at the Parameters class documentation, but I tried to 
+    give documentation to the important paramters throughout the layers documentation as well. 
+    """
+    def __init__(self, param: Parameters):
+        super().__init__(name="ConvTasNet")
+        self.param = param
+
+
+        self.encoder = Encoder(self.param)
+        self.separator = Separator(self.param)
+        self.decoder = Decoder(self.param)
+
+    def call(self, x):
+        w = self.encoder(x)
+        m_i = self.separator(w)
+
+        decoded = self.decoder(w, m_i)
+
+        return decoded
---
+++ mss_nn_implementation/loss.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+from tensorflow.keras.losses import Loss
+import math
+
+class LOGL2(Loss):
+    def __init__(self):
+        super(LOGL2, self).__init__(name="logl2")
+        self.eps = 1e-7
+    def call(self, s, s_hat): # (M, C, T)
+        # Calculate the L2 norm
+        l2_norm = tf.square(s - s_hat)
+        
+        # Calculate the base-10 logarithm of the L2 norm
+        log10_l2_norm = tf.math.log(l2_norm) / tf.math.log(tf.constant(10, dtype=l2_norm.dtype))
+        
+        return tf.reduce_mean(log10_l2_norm + self.eps, name='loss')
+    
+class SDSDR(Loss):
+    def __init__(self):
+        super(SDSDR, self).__init__(name="sdsdr")
+        self.eps = 1e-7  
+    def call(self, s, s_hat): # (M, C, T)
+        sources_energy = tf.reduce_sum(tf.square(s), axis=-1)
+
+        alpha = tf.reduce_sum(tf.multiply(s, s_hat), axis=-1) / (sources_energy+self.eps)
+
+        e_true = s
+        e_res = s_hat - e_true
+
+        signal = tf.reduce_sum(tf.square(e_true), axis=-1)
+
+        noise = tf.reduce_sum(tf.square(e_res), axis=-1)
+
+        snr = 10 * tf.math.log((signal + self.eps) / (noise + self.eps)) / tf.math.log(tf.constant(10, dtype=signal.dtype))  # Typical SNR not one specific to MSS
+
+        sd_sdr = snr + 10 * tf.math.log(tf.square(alpha)) / tf.math.log(tf.constant(10, dtype=signal.dtype))
+
+        return -tf.reduce_mean(sd_sdr)
+
+class SISDR(Loss):
+    def __init__(self):
+        super(SISDR, self).__init__(name="sisdr")
+        self.eps = 1e-7  
+    def call(self, s, s_hat): # (M, C, T)
+        sources_energy = tf.reduce_sum(tf.square(s), axis=-1, keepdims=True)
+        alpha = tf.reduce_sum(tf.multiply(s,s_hat), axis=-1, keepdims=True) / (sources_energy+self.eps)
+
+        e_true = tf.math.multiply(s, alpha)
+        e_res = s_hat - e_true
+
+        signal = tf.reduce_sum(tf.square(e_true), axis=-1, keepdims=True)
+        noise = tf.reduce_sum(tf.square(e_res), axis=-1, keepdims=True)
+
+        si_sdr = 10 * tf.math.log(signal / (noise + self.eps)) / tf.math.log(tf.constant(10, dtype=signal.dtype))
+
+        return -tf.reduce_mean(si_sdr, name='loss') # Loss functions typically minimize, hence the negative sign
+  
+class SISNR(Loss):
+    def __init__(self, eps=1e-7):
+        super(SISNR, self).__init__(name="sisnr")
+        self.eps = eps
+
+    def call(self, s, s_hat):
+        # Ensure input tensors are float for proper calculations
+        s = tf.cast(s, tf.float32)
+        s_hat = tf.cast(s_hat, tf.float32)
+
+        # Compute the target signal
+        s_target = s * (tf.tensordot(s, s_hat)/tf.norm(s)**2)
+        # Compute the error noise
+        e_noise = tf.math.subtract(s_hat, s_target)
+
+        # Compute SI-SNR
+        sisnr = 10 * tf.math.log((tf.norm(s_target)** 2 + self.eps) /
+                                 (tf.norm(e_noise)** 2 + self.eps)) / tf.math.log(10.0)
+        return -tf.reduce_mean(sisnr, name='loss')  # Loss functions typically minimize, hence the negative sign
+  
+class SDR(Loss):
+
+    def __init__(self, **kwargs):
+        super(SDR, self).__init__(**kwargs)
+        self.eps = 1e-7
+
+    def call(self, s, s_hat):
+        return 20 * tf.math.log(tf.norm(s_hat - s) / (tf.norm(s) + self.eps) + self.eps) / math.log(10) 
